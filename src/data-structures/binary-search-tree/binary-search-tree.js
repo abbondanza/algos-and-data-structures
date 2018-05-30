a34eccf2064ee448c7dc521798e8827a2feca9a2
Rafael Abbondanza <abbondanza.r@gmail.com>
2018-05-30 15:25:17 -0400

    adds tests for all methods & implements some - BST

--- src/data-structures/binary-search-tree/binary-search-tree.js
+++ src/data-structures/binary-search-tree/binary-search-tree.js
@@ -1,7 +1,114 @@
 import BinarySearchTreeNode from './binary-search-tree-node';
 
 export default class BinarySearchTree {
-    insert() { /* TBI */ }
-    contains() { /* TBI */ }
-    remove() { /* TBI */ }
+    constructor() {
+        this.root = null;
+    }
+    insert(value) {
+        const node = new BinarySearchTreeNode(value);
+        if(!this.root) {
+            this.root = node;
+            return;
+        }
+        return _insertNode(node, this.root, this.compare);
+    }
+
+    contains(value) {
+        return _contains(value, this.root, this.compare);
+    }
+
+    remove() {}
+    min() {
+        if(this.isEmpty()) return null;
+        return _findLeftMostNode(this.root).value;
+    }
+    max() {
+        if(this.isEmpty()) return null;
+        return _findRightMostNode(this.root).value;
+    }
+    getRoot() {
+        return this.root;
+    }
+    countNodes() {
+        return _countNodes(this.root, 0);
+    }
+    compare(nodeA, nodeB) {
+        if(nodeA.value < nodeB.value) {
+            return -1;
+        }
+        if(nodeA.value > nodeB.value) {
+            return 1;
+        }
+        return 0;
+    }
+    inOrder(node) {
+        if(!arguments.length) {
+            return this.inOrder(this.root);
+        }
+        if(!node) {
+            return;
+        }
+        this.inOrder(node.left);
+        console.log(node.value);
+        this.inOrder(node.right);
+    }
+    isEmpty() {
+        return !this.root;
+    }
+}
+
+function _findRightMostNode(root) {
+    let maxNode = root;
+    while(maxNode.right) {
+        maxNode = maxNode.right;
+    }
+    return maxNode;
+}
+
+function _findLeftMostNode(root) {
+    let minNode = root;
+    while(minNode.left) {
+        minNode = minNode.left;
+    }
+    return minNode;
+}
+
+function _countNodes(root) {
+    if(!root) {
+        return 0;
+    }
+    return _countNodes(root.left) + _countNodes(root.right) + 1;
+}
+
+function _insertNode(node, root, compareFn) {
+    const toLeft = (compareFn(node, root) < 0);
+    if(toLeft && !root.left) {
+        root.left = node;
+        return;
+    }
+
+    if(!toLeft && !root.right) {
+        root.right = node;
+        return;
+    }
+
+    if(toLeft) {
+        _insertNode(node, root.left, compareFn);
+    } else {
+        _insertNode(node, root.right, compareFn);
+    }
+}
+
+function _contains(value, node, compareFn) {
+    if(node === null) {
+        return false;
+    }
+    const compare = compareFn(new BinarySearchTreeNode(value), node);
+    if(compare === 0) {
+        return true;
+    } else if(compare < 0) {
+        return _contains(value, node.left, compareFn);
+    } else {
+        return _contains(value, node.right, compareFn);
+    }
 }
--- src/data-structures/binary-search-tree/binary-search-tree.test.js
+++ src/data-structures/binary-search-tree/binary-search-tree.test.js
@@ -1,3 +1,113 @@
 import BinarySearchTree from './binary-search-tree';
 
-test('nothing yet', () => {})
+test('isEmpty() on empty tree', () => {
+    const tree = new BinarySearchTree();
+    expect(tree.isEmpty()).toBe(true);
+})
+
+test('isEmpty() on non-empty tree', () => {
+    const tree = new BinarySearchTree();
+    tree.insert('R')
+    expect(tree.isEmpty()).toBe(false);
+})
+
+test('insert()', () => {
+    const tree = new BinarySearchTree();
+    tree.insert('M')
+    tree.insert('A')
+    tree.insert('Z');
+    tree.insert('O');
+    tree.insert('B');
+
+    expect(tree.countNodes()).toBe(5);
+    expect(tree.contains('M')).toBe(true);
+    expect(tree.contains('A')).toBe(true);
+    expect(tree.contains('Z')).toBe(true);
+    expect(tree.contains('O')).toBe(true);
+    expect(tree.contains('B')).toBe(true);
+})
+
+test('insert() duplicates', () => {
+    const tree = new BinarySearchTree();
+    tree.insert('M');
+    tree.insert('M');
+
+    expect(tree.countNodes()).toBe(2);
+    expect(tree.contains('M')).toBe(true);
+    expect(tree.contains('Z')).toBe(false);
+})
+test('contains()', () => {
+    const tree = new BinarySearchTree();
+    tree.insert('M');
+    tree.insert('A');
+    tree.insert('Z');
+
+    expect(tree.countNodes()).toBe(3);
+    expect(tree.contains('M')).toBe(true);
+    expect(tree.contains('A')).toBe(true);
+    expect(tree.contains('Z')).toBe(true);
+    expect(tree.contains('X')).toBe(false);
+})
+
+test('min()', () => {
+    const tree = new BinarySearchTree();
+    tree.insert('M');
+    tree.insert('B');
+    tree.insert('Z');
+    tree.insert('X');
+    tree.insert('A');
+
+    expect(tree.min()).toBe('A');
+})
+
+test('min() on empty tree', () => {
+    const tree = new BinarySearchTree();
+    expect(tree.min()).toBeNull();
+})
+test('max()', () => {
+    const tree = new BinarySearchTree();
+    tree.insert('M');
+    tree.insert('A');
+    tree.insert('Z');
+    tree.insert('X');
+
+    expect(tree.max()).toBe('Z');
+})
+
+test('max() on empty tree', () => {
+    const tree = new BinarySearchTree();
+    expect(tree.max()).toBeNull();
+})
+
+test.skip('remove()', () => {
+    const tree = new BinarySearchTree();
+    tree.insert('M');
+    tree.insert('A');
+    tree.insert('Z');
+    tree.insert('X');
+    tree.remove('M');
+    tree.remove('X');
+
+    expect(tree.countNodes()).toBe(2);
+    expect(tree.contains('M')).toBe(false);
+    expect(tree.contains('X')).toBe(false);
+})
+
+test.skip('remove() with duplicates', () => {
+    const tree = new BinarySearchTree();
+    tree.insert('A');
+    tree.insert('A');
+    tree.insert('A');
+
+    expect(tree.remove('A')).not.toThrow();
+    expect(tree.countNodes()).toBe(2);
+    expect(tree.contains('A')).toBe(true);
+})
+
+test.skip('remove() non-existent item', () => {
+    const tree = new BinarySearchTree();
+    tree.insert('A');
+    tree.insert('B');
+    tree.insert('C');
+    expect(tree.remove('Z')).not.toThrow();
+})
