0805fa4283d0b253e4884e28b7ea513ac41fce7b
Rafael Abbondanza <abbondanza.r@gmail.com>
2018-07-05 20:24:18 -0400

    ra | wraps up dijkstras shortest path algorithm

--- src/algorithms/graph/dijkstras-shortest-path/dijkstras-shortest-path.js
+++ src/algorithms/graph/dijkstras-shortest-path/dijkstras-shortest-path.js
@@ -1,37 +1,51 @@
 import bfs from '../bfs/bfs';
-import Queue from '../../../data-structures/queue/queue';
+import PriorityQueue from '../../../data-structures/priority-queue/priority-queue';
 
 const dijkstrasShortestPath = (src, dest) => {
-	// get list of all vertices
 	let dist = {};
-	let prev = {};
-	let visited = {};
-	let vertices = [];
+	let vertices = {};
+
+	// loop through all the vertices to instantiate maps
 	bfs(src, (v) => {
-		vertices.push(v);
-		dist[v.key()] = +Infinity;
-		prev[v.key()] = null;
-		visited[v.key()] = false;
+		let vKey = v.key();
+		vertices[vKey] = v;
+		dist[vKey] = +Infinity;
 	});
 
-	dist[src.key()] = 0;
-	let Q = new Queue();
-	Q.enqueue(src);
+	// create priority queue
+	// (a => 1 just means that it won't compare the values of two vertices with the same priority)
+	let pq = new PriorityQueue(a => 1);
 
-	while(!Q.isEmpty()) {
-		// for each neighbor, update dist & visited if needed
-		let vertex = Q.dequeue();
-		visited[vertex.key()] = true;
-		vertex.edges.map((edge) => {
-			let eKey = edge.end.key();
-			if(edge.weight < dist[eKey]) {
-				dist[eKey] = edge.weight;
-				prev[eKey] = vertex;
+	// set dist to src as 0 & insert it in priority queue
+	dist[src.key()] = 0;
+	pq.insert(src.key(), 0);
+	let visited = {};
+
+	// BFS traversal, but using priority queue instead
+	while(!pq.isEmpty()) {
+		let vKey = pq.poll();
+		let currVertex = vertices[vKey];
+		visited[vKey] = true;
+
+		// for each vertex, visit all its neighbors
+		for(let i = 0; i < currVertex.edges.length; i++) {
+			let edge = currVertex.edges[i];
+			let uKey = edge.end.key();
+			// `continue` if visited already
+			if(visited[uKey]) continue;
+
+			// if the current path length is shorter than prev path length,
+			// then update dist
+			let currLen = dist[vKey] + edge.weight;
+			if(currLen < dist[uKey]) {
+				dist[uKey] = currLen;
+				pq.insert(uKey, currLen);
 			}
-			Q.enqueue(edge.end);
-		})
+		}
 	}
-	return 0;
+
+	// is there a way to exit sooner?
+	return dist[dest.key()];
 };
 
 export default dijkstrasShortestPath;
--- src/algorithms/graph/dijkstras-shortest-path/dijkstras-shortest-path.test.js
+++ src/algorithms/graph/dijkstras-shortest-path/dijkstras-shortest-path.test.js
@@ -9,8 +9,9 @@ test('dijkstrasShortestPath()', () => {
 	graph.addEdge('B', 'C', 1);
 	graph.addEdge('B', 'D', 5);
 
-	graph.addEdge('C', 'D', 1);
-	graph.addEdge('C', 'E', 1);
+	graph.addEdge('C', 'D', 2);
+	graph.addEdge('C', 'E', 3);
+
 	graph.addEdge('D', 'E', 1);
 
 	/*
@@ -26,7 +27,7 @@ test('dijkstrasShortestPath()', () => {
 	expect(dijkstrasShortestPath(
 		graph.getVertex('A'),
 		graph.getVertex('E')
-	)).toBe(6);
+	)).toBe(5);
 
 	expect(dijkstrasShortestPath(
 		graph.getVertex('A'),
@@ -38,4 +39,65 @@ test('dijkstrasShortestPath()', () => {
 		graph.getVertex('D')
 	)).toBe(4);
 
+})
+
+test('dijkstrasShortestPath()', () => {
+	let graph = new Graph();
+	graph.addEdge('A', 'C', 8);
+	graph.addEdge('A', 'B', 1);
+
+	graph.addEdge('B', 'D', 1);
+
+	graph.addEdge('C', 'E', 8);
+
+	graph.addEdge('D', 'E', 8);
+	graph.addEdge('D', 'F', 9);
+
+	graph.addEdge('E', 'F', 2);
+	/*
+		Graph:
+
+		A --8-- C --8-- E 
+		|             / |
+		1           8   2
+		|         /     |
+		B --1-- D --9-- F
+	*/
+
+	expect(dijkstrasShortestPath(
+		graph.getVertex('A'),
+		graph.getVertex('E')
+	)).toBe(10);
+
+	expect(dijkstrasShortestPath(
+		graph.getVertex('A'),
+		graph.getVertex('F')
+	)).toBe(11);
+})
+
+
+test('dijkstrasShortestPath() - non-greedy path', () => {
+	let graph = new Graph();
+	graph.addEdge('A', 'C', 1);
+	graph.addEdge('A', 'B', 2);
+
+	graph.addEdge('B', 'F', 1);
+
+	graph.addEdge('F', 'E', 1);
+
+	graph.addEdge('C', 'E', 9);
+	/*
+		Graph:
+
+		A --1-- C --9-- E 
+		|               |
+		2               1
+		|               |
+		B ------1------ F
+	*/
+
+	expect(dijkstrasShortestPath(
+		graph.getVertex('A'),
+		graph.getVertex('E')
+	)).toBe(4);
 })
