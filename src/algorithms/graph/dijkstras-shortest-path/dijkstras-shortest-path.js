63b6f4215ed52cf76bd34d919fe475cf8f8177c0
Rafael Abbondanza <abbondanza.r@gmail.com>
2018-07-19 12:01:45 -0400

    ra |shortest path commentary

--- src/algorithms/graph/dijkstras-shortest-path/dijkstras-shortest-path.js
+++ src/algorithms/graph/dijkstras-shortest-path/dijkstras-shortest-path.js
@@ -5,20 +5,24 @@ const dijkstrasShortestPath = (src, dest) => {
 	let dist = {};
 	let vertices = {};
 
-	// loop through all the vertices to instantiate maps
-	bfs(src, (v) => {
-		let vKey = v.key();
-		vertices[vKey] = v;
-		dist[vKey] = +Infinity;
-	});
-
 	// create priority queue
 	// (a => 1 just means that it won't compare the values of two vertices with the same priority)
 	let pq = new PriorityQueue(a => 1);
 
-	// set dist to src as 0 & insert it in priority queue
+	// loop through all the vertices to instantiate maps
+	// set distance to all vertices as Infinity
+	bfs(src, (v) => {
+		let vKey = v.key();
+		vertices[vKey] = v;
+		dist[vKey] = +Infinity;
+		pq.insert(vKey, dist[vKey]);
+	});
+	// except source's
 	dist[src.key()] = 0;
-	pq.insert(src.key(), 0);
+
+	//make sure to update its priority
+	pq.changePriority(src.key(), 0);
+
 	let visited = {};
 
 	// BFS traversal, but using priority queue instead
@@ -28,20 +32,20 @@ const dijkstrasShortestPath = (src, dest) => {
 		visited[vKey] = true;
 
 		// for each vertex, visit all its neighbors
-		for(let i = 0; i < currVertex.edges.length; i++) {
-			let edge = currVertex.edges[i];
-			let uKey = edge.end.key();
-			// `continue` if visited already
-			if(visited[uKey]) continue;
+		currVertex.edges.map((E) => {
+			let destKey = E.end.key();
+			// don't bother visiting it if it has already been visited
+			if(visited[destKey]) return;
 
-			// if the current path length is shorter than prev path length,
-			// then update dist
-			let currLen = dist[vKey] + edge.weight;
-			if(currLen < dist[uKey]) {
-				dist[uKey] = currLen;
-				pq.insert(uKey, currLen);
+			// be greedy! the next one we want to visit is the one with the shortest path
+			let distToNext = dist[vKey] + E.weight;
+			// if we have found a shorter path to next vertex
+			// then update priority queue
+			if(distToNext < dist[destKey]) {
+				dist[destKey] = distToNext;
+				pq.changePriority(destKey, dist[destKey]);
 			}
-		}
+		});
 	}
 
 	// is there a way to exit sooner?
