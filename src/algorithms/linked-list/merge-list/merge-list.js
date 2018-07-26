88e2777ebda7ff4b91a8a085287f8f47d3815165
Rafael Abbondanza <abbondanza.r@gmail.com>
2018-07-26 07:57:29 -0400

    ra | merge linked lists in place (wip)

---
+++ src/algorithms/linked-list/merge-list/merge-list.js
@@ -0,0 +1,10 @@
+/*
+	Given two sorted lists, merge them in place.
+	Returns pointer to sorted list contining all items.
+*/
+
+const mergeLists = (l1, l2) => {
+	return l1;
+};
+
+export default mergeLists;
