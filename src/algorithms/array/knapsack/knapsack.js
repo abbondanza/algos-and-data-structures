8adaa6f2b103b9be41e736e49f9d2d46f97f6252
Rafael Abbondanza <abbondanza.r@gmail.com>
2018-08-20 21:51:00 -0400

    ra | more comments

--- src/algorithms/array/knapsack/knapsack.js
+++ src/algorithms/array/knapsack/knapsack.js
@@ -3,7 +3,7 @@
 
     Given a list of items ({ weight, value }) and a knapsack
     weightLimit, calculate maximum sum of values in knapsack
-    whose total weights are less than weightLimit.
+    whose total weights are less or equal to weightLimit.
 */
 
 const knapsackRecursive = (items, weightLimit, n) => {
@@ -38,27 +38,27 @@ const knapsackDynamic = (items, weightLimit) => {
 
     // Iterate through rows and columns to fill K matrix.
     // K[r][c] holds max knapsack value for subproblem
-    // where weightLimit = c and items.length = r.
+    // where r = items.length and c = weightLimit.
     for(let row=1; row<=items.length; row++) {
-        for(let col=1;col<=weightLimit; col++) {
+        for(let currLimit=1; currLimit<=weightLimit; currLimit++) {
             let currItem = items[row-1];
             let cellVal = 0;
-            // Current item does not fit into knapsack,
-            // so copy value from cell to the the top.
-            if(currItem.weight > col) {
-                cellVal = K[row-1][col];
-            } else {
+            // Current item *certainly* does not fit into knapsack
+            // Skip it and copy value from cell to the the top.
+            if(currItem.weight > currLimit) {
+                cellVal = K[row-1][currLimit];
+            } else { // Current item *might* fit into knapsack
                 // Maximize current cell value by finding max
                 // between cases:
                 //      1) item goes into knapsack
                 //      2) item stays out of knapsack
                 cellVal = Math.max(
-                    currItem.value + K[row-1][col-currItem.weight],
-                    K[row-1][col]
+                    currItem.value + K[row-1][currLimit-currItem.weight],
+                    K[row-1][currLimit]
                 );
             }
 
-            K[row][col] = cellVal;
+            K[row][currLimit] = cellVal;
         }
     }
 
