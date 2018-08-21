2193faef5c269e07c4c961129e40a871aff3a1ba
Rafael Abbondanza <abbondanza.r@gmail.com>
2018-08-20 22:14:56 -0400

    ra | comments to max subarray and threesum

--- src/algorithms/array/max-subarray-sum/max-subarray-sum.js
+++ src/algorithms/array/max-subarray-sum/max-subarray-sum.js
@@ -1,15 +1,29 @@
+/*
+    Problem description:
+
+    Given an array (arr) of numbers, find the
+    max sum of contiguous elements in array.
+*/
+
 const maxSubarraySum = (arr) => {
     let maxSum = -Infinity;
-    let curr = -Infinity;
-    arr.map((item) => {
-        if(curr + item > item) {
-            curr += item;
-        } else {
-            curr = item;
+    let currSum = -Infinity;
+
+    for(let i=0; i<arr.length; i++) {
+        let item = arr[i];
+        // if existing sum + current item
+        // increases existing sum, then add
+        // it to existing sum
+        if(currSum + item > item) {
+            currSum += item;
+        }
+        // if current item is better off alone...
+        else {
+            currSum = item;
         }
 
-        maxSum = Math.max(curr, maxSum);
-    });
+        maxSum = Math.max(currSum, maxSum);
+    }
 
     return maxSum;
 }
--- src/algorithms/array/threesum/threesum.js
+++ src/algorithms/array/threesum/threesum.js
@@ -1,22 +1,29 @@
-const threesum = (arr, n) => {
+/*
+	Problem description:
+
+	Given an array (arr), find three elements in
+	array that add up to given n.
+*/
+const threesum = (arr, sum) => {
 
 	for(let i=0; i<arr.length; i++) {
-		let goal = n - arr[i];
-		let dict = {};
-		let sum3 = [arr[i]];
-		// This is now a simpler 2-sum problem
+		// the goal is now to find a pair in
+		// rest of array that adds to target
+		// ~ we can do that in O(n) ~
+		let target = sum - arr[i];
+		let diffs = {};
 		for(let j=0; j<arr.length; j++) {
-			if(i === j) continue;
-			if(dict[arr[j]]) {
-				sum3.push(arr[j]);
-				sum3.push(arr[dict[arr[j]]]);
-				return sum3;
+			if(j === i) continue;
+			if(diffs[arr[j]] !== void 0) {
+				return [arr[i], arr[j], arr[diffs[arr[j]]]];
 			}
 
-			dict[goal - arr[j]] = j;
+			diffs[target - arr[j]] = j;
 		}
 	}
+
 	return [];
-}
+
+};
 
 export default threesum;
